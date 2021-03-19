d162c4af46b76677a47a36e7cfc5b42561094c7a
tapinautic <80972931+tapinautic@users.noreply.github.com>
2021-03-19 17:08:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  $('.fa-bars').click(function(){
+      $(this).toggleClass('fa-times');
+      $('nav').toggleClass('nav-toggle');
+  });
+
+  $('nav ul li a').click(function(){
+      $('.fa-bars').removeClass('fa-times');
+      $('nav').removeClass('nav-toggle');
+  });
+});
+
+$(window).on('scroll load', function(){
+  if ($(window).scrollTop() > 20) {
+      $('#header').addClass('header-active');
+  }
+  else {
+      $('#header').removeClass('header-active');
+  }
+});
